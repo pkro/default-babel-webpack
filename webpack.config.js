4010741946085d0c8b557b4d503872d9b792ee51
pkro <pkrombach@web.de>
2020-03-01 17:34:03 +0100

    added webpack and moved babel conf to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -12,4 +12,28 @@ module.exports = {
 		path: PATH_DIST,
 		filename: 'js/[name].[hash].js',
 	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/, // apply this rule to all js files
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							['@babel/preset-env', {
+								debug: true, // output targets / plugins when compiling
+								useBuiltIns: 'usage', // only load polyfills when necessary
+								corejs: 3, // must match installed version in package.json
+								// targets: "" //(already done in package.json)
+							}]
+						]
+					}
+				}
+			}
+		]
+	}
 };
+
+
